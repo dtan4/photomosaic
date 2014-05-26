9911a921f0d8a13aca5c692cc56b7852584c9544
dtan4 <dtanshi45@gmail.com>
2014-05-27 00:29:44 +0900

    Implement CLI

--- lib/photomosaic/client.rb
+++ lib/photomosaic/client.rb
@@ -3,12 +3,35 @@ module Photomosaic
     def self.run(options)
       search_engine = options[:search_engine].new(options[:api_key], options[:number_results])
       image_url_list = search_engine.get_image_list(options[:keyword])
-      image_downloader = ImageDownloader.new
+      base_image = Photomosaic::Image.preprocess_image(options[:base_image], options[:width], options[:height], 4, options[:number_colors])
+      image_downloader = Photomosaic::ImageDownloader.new
 
       begin
         image_path_list = image_downloader.download_images(image_url_list)
+        image_list = get_image_list(image_path_list)
+        images = base_image.dispatch_images(image_list, 1, 2, options[:color_model])
+        resize_to_pixel(images)
+        Photomosaic::Image.create_mosaic_image(images, options[:output])
       ensure
         image_downloader.remove_save_dir
+      end
+    end
+
+    private
+
+    def self.get_image_list(image_path_list)
+      image_path_list.map do |path|
+        begin
+          Photomosaic::Image.new(path)
+        rescue
+          nil
+        end
+      end.compact
+    end
+
+    def self.resize_to_pixel(images)
+      images.map! do |row|
+        row.map {|image| image.resize!(40, 20, false); image }
       end
     end
   end
--- spec/photomosaic/client_spec.rb
+++ spec/photomosaic/client_spec.rb
@@ -1,38 +1,74 @@
 require "spec_helper"
+require "fileutils"
 
 module Photomosaic
   describe Client do
-    let(:search_engine) do
-      SearchEngine::Bing
-    end
-
-    let(:api_key) do
-      "api_key"
-    end
-
-    let(:keyword) do
-      "keyword"
-    end
-
-    let(:number_results) do
-      30
-    end
+    let(:api_key) { "api_key" }
+    let(:base_image) { fixture_path("lena.png") }
+    let(:color_model) { :rgb }
+    let(:height) { 200 }
+    let(:keyword) { "keyword" }
+    let(:number_colors) { 16 }
+    let(:number_results) { 50 }
+    let(:output_path) { tmp_path("output.png") }
+    let(:search_engine) { SearchEngine::Bing }
+    let(:width) { 200 }
 
     let(:options) do
       {
        api_key: api_key,
-       search_engine: search_engine,
+       base_image: base_image,
+       color_model: color_model,
+       height: height,
        keyword: keyword,
-       number_results: number_results
+       number_colors: number_colors,
+       number_results: number_results,
+       output: output_path,
+       search_engine: search_engine,
+       width: width
       }
     end
 
+    let(:image_name_list) do
+      (0..5).map { |i| "lena_#{i}.png" }
+    end
+
+    let(:image_path_list) do
+      image_name_list.map { |name| fixture_path(name) }
+    end
+
+    let(:image_url_list) do
+      image_name_list.map { |name| "http://example.com/#{name}" }
+    end
+
+    let(:dispatched_images) do
+      5.times.inject([]) do |images, _|
+        # TODO: Use mock
+        images << image_path_list.map { |path| Photomosaic::Image.new(path) }
+        images
+      end
+    end
+
     describe "#run" do
+      before do
+        allow_any_instance_of(Photomosaic::SearchEngine::Bing).to receive(:get_image_list)
+          .with(keyword).and_return(image_url_list)
+        allow_any_instance_of(Photomosaic::ImageDownloader).to receive(:download_images)
+          .and_return(image_path_list)
+        allow_any_instance_of(Photomosaic::Image).to receive(:dispatch_images)
+          .and_return(dispatched_images)
+
+        FileUtils.rm_rf(tmp_dir) if Dir.exist?(tmp_dir)
+        Dir.mkdir(tmp_dir)
+      end
+
       it "should run the program" do
-        expect_any_instance_of(SearchEngine::Bing).to receive(:get_image_list).with(keyword).once
-        expect_any_instance_of(ImageDownloader).to receive(:download_images).once
-        expect_any_instance_of(ImageDownloader).to receive(:remove_save_dir).once
         described_class.run(options)
+        expect(File.exist?(output_path)).to be_true
+      end
+
+      after do
+        FileUtils.rm_rf(tmp_dir)
       end
     end
   end
