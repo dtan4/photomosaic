6cf2a95d8c3ea01c1764bc19e83d06394b28d356
dtan4 <dtanshi45@gmail.com>
2014-05-27 19:12:32 +0900

    Add -b and -o options

--- lib/photomosaic/options.rb
+++ lib/photomosaic/options.rb
@@ -15,13 +15,17 @@ module Photomosaic
     width
     )
 
+    REQUIRED_KEYS = %w(
+    api_key
+    base_image
+    output_path
+    )
+
     def self.parse(argv)
       options = default_options
-      argv = parser(options).parse(argv)
-
-      options[:base_image] = File.expand_path(argv[0])
-      options[:output_path] = File.expand_path(argv[1])
+      parser(options).parse(argv)
       options[:api_key] = api_key
+      check_options(options)
 
       self.new(options)
     end
@@ -40,6 +44,12 @@ module Photomosaic
       ENV["PHOTOMOSAIC_API_KEY"]
     end
 
+    def self.check_options(options)
+      REQUIRED_KEYS.each do |key|
+        raise OptionParser::MissingArgument unless options[key.to_sym]
+      end
+    end
+
     def self.default_options
       {
        color_model: :rgb,
@@ -53,9 +63,11 @@ module Photomosaic
 
     def self.parser(options)
       OptionParser.new do |opt|
+        opt.on("-b", "--base_image=VAL", "Path of base image") { |val| options[:base_image] = File.expand_path(val) }
         opt.on("-c", "--colors=VAL", "Number of colors") { |val| options[:colors] = val.to_i }
         opt.on("-h", "--height=VAL", "Height of output image") { |val| options[:height] = val.to_i }
         opt.on("-k", "--keyword=VAL", "Keyword") { |val| options[:keyword] = val }
+        opt.on("-o", "--output_path=VAL", "Path of mosaic image") { |val| options[:output_path] = File.expand_path(val) }
         opt.on("-r", "--results=VAL", "Number of results") { |val| options[:results] = val.to_i }
         opt.on("-w", "--width=VAL", "Width of output image") { |val| options[:width] = val.to_i }
         opt.on("--hsv", "Use HSV") { |val| options[:color_model] = :hsv }
--- spec/photomosaic/options_spec.rb
+++ spec/photomosaic/options_spec.rb
@@ -11,31 +11,45 @@ module Photomosaic
     let(:keyword) { "keyword" }
     let(:output_path) { "output_path" }
 
-    let(:argv) do
-      "#{base_image_path} #{output_path} -k #{keyword} -c #{colors} -h #{height} -w #{width} -r #{results}".split(" ")
-    end
-
     describe "#parse" do
-      before do
-        allow(ENV).to receive(:[]).with("PHOTOMOSAIC_API_KEY").and_return(api_key)
+      context "when the required parameters are specified" do
+        let(:argv) do
+          "-b #{base_image_path} -o #{output_path} -k #{keyword} -c #{colors} -h #{height} -w #{width} -r #{results}".split(" ")
+        end
+
+        before do
+          allow(ENV).to receive(:[]).with("PHOTOMOSAIC_API_KEY").and_return(api_key)
+        end
+
+        it "should return Options instance" do
+          expect(described_class.parse(argv)).to be_a described_class
+        end
+
+        it "should parse option string" do
+          options = described_class.parse(argv)
+          expect(options.api_key).to eq api_key
+          expect(options.base_image).to eq File.expand_path(base_image_path)
+          expect(options.color_model).to eq :rgb
+          expect(options.colors).to eq colors
+          expect(options.height).to eq height
+          expect(options.keyword).to eq keyword
+          expect(options.output_path).to eq File.expand_path(output_path)
+          expect(options.results).to eq results
+          expect(options.search_engine).to eq Photomosaic::SearchEngine::Bing
+          expect(options.width).to eq width
+        end
       end
 
-      it "should return Options instance" do
-        expect(described_class.parse(argv)).to be_a described_class
-      end
+      context "when the required parameters are not specified" do
+        let(:argv) do
+          "-b #{base_image_path}"
+        end
 
-      it "should parse option string" do
-        options = described_class.parse(argv)
-        expect(options.api_key).to eq api_key
-        expect(options.base_image).to eq File.expand_path(base_image_path)
-        expect(options.color_model).to eq :rgb
-        expect(options.colors).to eq colors
-        expect(options.height).to eq height
-        expect(options.keyword).to eq keyword
-        expect(options.output_path).to eq File.expand_path(output_path)
-        expect(options.results).to eq results
-        expect(options.search_engine).to eq Photomosaic::SearchEngine::Bing
-        expect(options.width).to eq width
+        it "should raise OptionParser::MissingArgument" do
+          expect do
+            described_class.parse(argv)
+          end.to raise_error OptionParser::MissingArgument
+        end
       end
     end
   end
