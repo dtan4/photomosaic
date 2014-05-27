2d9930848f92ef61ee81ec13195d934a86211081
dtan4 <dtanshi45@gmail.com>
2014-05-27 16:19:51 +0900

    Use Options class to parse arguments string

--- lib/photomosaic/client.rb
+++ lib/photomosaic/client.rb
@@ -1,17 +1,18 @@
 module Photomosaic
   class Client
-    def self.run(options)
-      search_engine = options[:search_engine].new(options[:api_key], options[:number_results])
-      image_url_list = search_engine.get_image_list(options[:keyword])
-      base_image = Photomosaic::Image.preprocess_image(options[:base_image], options[:width], options[:height], 4, options[:number_colors])
+    def self.run(argv)
+      options = Photomosaic::Options.parse(argv)
+      search_engine = options.search_engine.new(options.api_key, options.results)
+      image_url_list = search_engine.get_image_list(options.keyword)
+      base_image = Photomosaic::Image.preprocess_image(options.base_image, options.width, options.height, 4, options.number_colors)
       image_downloader = Photomosaic::ImageDownloader.new
 
       begin
         image_path_list = image_downloader.download_images(image_url_list)
         image_list = get_image_list(image_path_list)
-        images = base_image.dispatch_images(image_list, 1, 2, options[:color_model])
+        images = base_image.dispatch_images(image_list, 1, 2, options.color_model)
         resize_to_pixel(images)
-        Photomosaic::Image.create_mosaic_image(images, options[:output])
+        Photomosaic::Image.create_mosaic_image(images, options.output_path)
       ensure
         image_downloader.remove_save_dir
       end
--- spec/photomosaic/client_spec.rb
+++ spec/photomosaic/client_spec.rb
@@ -1,16 +1,17 @@
 require "spec_helper"
 require "fileutils"
+require "ostruct"
 
 module Photomosaic
   describe Client do
     let(:api_key) { "api_key" }
     let(:base_image) { fixture_path("lena.png") }
     let(:color_model) { :rgb }
+    let(:colors) { 16 }
     let(:height) { 200 }
     let(:keyword) { "keyword" }
-    let(:number_colors) { 16 }
-    let(:number_results) { 50 }
     let(:output_path) { tmp_path("output.png") }
+    let(:results) { 50 }
     let(:search_engine) { SearchEngine::Bing }
     let(:width) { 200 }
 
@@ -19,15 +20,17 @@ module Photomosaic
        api_key: api_key,
        base_image: base_image,
        color_model: color_model,
+       colors: colors,
        height: height,
        keyword: keyword,
-       number_colors: number_colors,
-       number_results: number_results,
-       output: output_path,
+       output_path: output_path,
+       results: results,
        search_engine: search_engine,
        width: width
       }
     end
+
+    let(:argv) { "argv" }
 
     let(:image_name_list) do
       (0..5).map { |i| "lena_#{i}.png" }
@@ -41,6 +44,11 @@ module Photomosaic
       image_name_list.map { |name| "http://example.com/#{name}" }
     end
 
+    let(:preprocessed_image) do
+      # TODO: Use mock
+      Photomosaic::Image.new(base_image)
+    end
+
     let(:dispatched_images) do
       5.times.inject([]) do |images, _|
         # TODO: Use mock
@@ -51,8 +59,10 @@ module Photomosaic
 
     describe "#run" do
       before do
+        allow(Photomosaic::Options).to receive(:parse).and_return(OpenStruct.new(options))
         allow_any_instance_of(Photomosaic::SearchEngine::Bing).to receive(:get_image_list)
           .with(keyword).and_return(image_url_list)
+        allow(Photomosaic::Image).to receive(:preprocess_image).and_return(preprocessed_image)
         allow_any_instance_of(Photomosaic::ImageDownloader).to receive(:download_images)
           .and_return(image_path_list)
         allow_any_instance_of(Photomosaic::Image).to receive(:dispatch_images)
@@ -63,7 +73,7 @@ module Photomosaic
       end
 
       it "should run the program" do
-        described_class.run(options)
+        described_class.run(argv)
         expect(File.exist?(output_path)).to be_true
       end
 
