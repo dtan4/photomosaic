226127bca6b796fd088842b0948fdc01277590a0
dtan4 <dtanshi45@gmail.com>
2014-05-28 12:47:08 +0900

    Add specs for RGB#to_hsv (Resolve #1)

--- spec/photomosaic/color/rgb_spec.rb
+++ spec/photomosaic/color/rgb_spec.rb
@@ -13,12 +13,38 @@ module Photomosaic
       end
 
       describe "#to_hsv" do
-        it "should convert RGB to HSV" do
+        # cf. http://tech-unlimited.com/color.html
+
+        it "should convert (50, 50, 50) to (0, 0, 20)" do
+          rgb = described_class.new(50, 50, 50)
+          hsv = rgb.to_hsv
+          expect(hsv.hue).to eq 0
+          expect(hsv.saturation).to be_within(1).of(0)
+          expect(hsv.value).to be_within(1).of(20)
+        end
+
+        it "should convert (50, 100, 200) to (220, 75, 78)" do
           rgb = described_class.new(50, 100, 200)
           hsv = rgb.to_hsv
           expect(hsv.hue).to eq 220
-          expect(hsv.saturation).to be_within(0.5).of(75.0)
-          expect(hsv.value).to be_within(0.5).of(78.0)
+          expect(hsv.saturation).to be_within(1).of(75)
+          expect(hsv.value).to be_within(1).of(78)
+        end
+
+        it "should convert (100, 50, 200) to (260, 75, 78)" do
+          rgb = described_class.new(100, 50, 200)
+          hsv = rgb.to_hsv
+          expect(hsv.hue).to eq 260
+          expect(hsv.saturation).to be_within(1).of(75)
+          expect(hsv.value).to be_within(1).of(78)
+        end
+
+        it "should convert (100, 200, 50) to (100, 75, 78)" do
+          rgb = described_class.new(100, 200, 50)
+          hsv = rgb.to_hsv
+          expect(hsv.hue).to eq 100
+          expect(hsv.saturation).to be_within(1).of(75)
+          expect(hsv.value).to be_within(1).of(78)
         end
       end
     end
